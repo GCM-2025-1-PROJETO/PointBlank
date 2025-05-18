d7ab349df3c70ff297a4dcddd982aa6108f37c6b
João Victor <joaovictors2010@gmail.com>
2025-05-18 20:47:53 -0300

    feature: retornar saldo

--- src/main/kotlin/br/imd/gcm/PointBlank/controllers/AccountController.kt
+++ src/main/kotlin/br/imd/gcm/PointBlank/controllers/AccountController.kt
@@ -38,6 +38,16 @@ class AccountController(private val accountService: AccountService) {
     }
 
     @PostMapping("/request")
+    fun create(): ResponseEntity<Any> {
+        return try {
+            val balance = accountService.getBalance(id)
+            ResponseEntity.ok(mapOf("balance" to balance))
+        } catch (e: EntityNotFoundException) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/request")
     fun create(@RequestBody request: AccountCreationRequest): ResponseEntity<Any> {
         return try {
             val account: Account = when (request.type.lowercase()) {
