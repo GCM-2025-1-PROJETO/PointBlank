06b5cf8b02e66acac38f3f35f4c3f4b70b1049e5
Pablo Deyvid de Paiva <pablo.paiva.123@ufrn.edu.br>
2025-05-18 18:04:55 -0300

    feature: inicializando projeto com spring initializr

---
+++ src/test/kotlin/br/imd/gcm/PontBlank/PontBlankApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.imd.gcm.PontBlank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontBlankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
