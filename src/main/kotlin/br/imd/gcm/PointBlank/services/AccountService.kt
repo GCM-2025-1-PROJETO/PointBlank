bb88413bc19ffba9375dd30acb6214d37877f14f
João Victor <joaovictors2010@gmail.com>
2025-05-22 17:44:27 -0300

    feature: conta bônus

--- src/main/kotlin/br/imd/gcm/PointBlank/services/AccountService.kt
+++ src/main/kotlin/br/imd/gcm/PointBlank/services/AccountService.kt
@@ -5,6 +5,12 @@ import br.imd.gcm.PointBlank.model.Account
 import br.imd.gcm.PointBlank.repositories.AccountRepository
 import org.springframework.stereotype.Service
 import br.imd.gcm.PointBlank.exception.DuplicateAccountException
+import br.imd.gcm.PointBlank.exception.InsufficientBalanceException
+import br.imd.gcm.PointBlank.exception.InvalidTransferAmountException
+import br.imd.gcm.PointBlank.model.BonusAccount
+import br.imd.gcm.PointBlank.model.dto.AmountTransferDTO
+import br.imd.gcm.PointBlank.model.dto.AmountTransferResponse
+import br.imd.gcm.PointBlank.repositories.BonusAccountRepository
 import java.math.BigDecimal
 
 @Service
@@ -99,6 +105,40 @@ class AccountService(
 
         account.balance -= amount
         return accountRepository.save(account)
+    }
+
+    fun transfer(transferRequest: AmountTransferDTO): AmountTransferResponse {
+        if (transferRequest.amount <= 0) {
+            throw InvalidTransferAmountException("O valor da transferência deve ser maior que zero.")
+        }
+
+        val originAccount = accountRepository.findById(transferRequest.originAccountNumber)
+            .orElseThrow { AccountNotFoundException("Conta de origem não encontrada") }
+        val targetAccount = accountRepository.findById(transferRequest.targetAccountNumber)
+            .orElseThrow { AccountNotFoundException("Conta de destino não encontrada") }
+
+        if (originAccount.balance < transferRequest.amount) {
+            throw InsufficientBalanceException("Saldo insuficiente para a transferência")
+        }
+
+        originAccount.balance -= transferRequest.amount
+
+        targetAccount.balance += transferRequest.amount
+        if (targetAccount is BonusAccount) {
+            targetAccount.points += (transferRequest.amount / 200).toInt()
+            bonusAccountRepository.save(targetAccount)
+        } else {
+            accountRepository.save(targetAccount)
+        }
+
+        accountRepository.save(originAccount)
+
+        return AmountTransferResponse(
+            originAccountNumber = originAccount.number,
+            originAccountBalance = originAccount.balance,
+            targetAccountNumber = targetAccount.number,
+            targetAccountBalance = targetAccount.balance
+        )
     }
 
 }
